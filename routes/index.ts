9b2a420298be61aa235ed5a1c7ed5d650d5a6e13
Chris Uth <uthtchris@gmail.com>
2017-08-21 19:18:45 -0400

    login/signup/displaying

--- routes/index.ts
+++ routes/index.ts
@@ -4,7 +4,35 @@ var router = Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-	res.render("index", { title: "Express" });
+	let theUser = {
+		username: "chris",
+		coins: 100
+	};
+
+	let theInventory = [
+		{
+			gridSpace: 5,
+			userGrid: 3,
+			locationY: 1,
+			locationX: 1,
+			userMonster: 135,
+			gridSpaceType: 3
+		}
+	];
+
+	if ( theUser != null) {
+		res.render("index", { user : theUser, inventory: theInventory });
+	} else { res.redirect("login"); }
+});
+
+// login page
+router.get("/login", function(req, res, next) {
+	res.render("login");
+});
+
+// sign up page
+router.get("/signup", function(req, res, next) {
+	res.render("signup");
 });
 
 module.exports = router;
