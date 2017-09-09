1fd5278805c5a51495df16bcc89073a6a239dd62
shazard <shaunh1096@gmail.com>
2017-09-08 20:52:51 -0400

    Deleted old models
    deleted old models

--- oldModels/Items.ts
+++
@@ -1,9 +0,0 @@
-import {Table, Column, Model, PrimaryKey, AutoIncrement, HasMany} from "sequelize-typescript";
-
-@Table
-export default class Items extends Model<Items> {
-	@Column({primaryKey: true})
-	public ItemID: number;
-	@Column
-	public ItemName: string;
-}
