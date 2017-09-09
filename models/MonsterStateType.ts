14b8d480208ebbbf94f40f0d4c6ab80ba36df0dd
shazard <shaunh1096@gmail.com>
2017-09-09 19:43:42 -0400

    Created monster and item forms
    followed same layout for user form

--- models/MonsterStateType.ts
+++ models/MonsterStateType.ts
@@ -5,6 +5,8 @@ export default class MonsterStateType extends Model<MonsterStateType> {
 
 	@Column
 	public Name: string;
+	// note: by default, monster states are a value of 1-100, with a default starting value of 25
+	// this can be changed or customized as needed upon intial setup, for example if administrator wishes to add Hit Points as a state
 	@Column
 	public MaxValue: number;
 	@Column
