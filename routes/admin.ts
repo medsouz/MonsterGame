f88c582b0f013ec16c436e3d037376cb5a2dc2b4
medsouz <medsouz99@gmail.com>
2017-09-09 17:22:04 -0400

    Add edit and new forms for admin panel

--- routes/admin.ts
+++ routes/admin.ts
@@ -3,9 +3,11 @@ import User from "../models/User";
 import {eAccountType} from "../models/User";
 import MonsterType from "../models/MonsterType";
 import Item from "../models/Item";
+import * as path from "path";
+import {Model} from "sequelize-typescript";
 var router = Router();
 
-// Only allow authenticated administrators onto admin pages
+// Only allow authenticated administrators onto admin panel
 router.use(function(req, res, next) {
 	if (req.user && req.user.AccountType === eAccountType.Admin)
 		next();
@@ -22,6 +24,65 @@ router.get("/", function(req, res, next) {
 				});
 			});
 		});
+});
+
+router.get("/new/:type", function(req, res, next) {
+	console.log(req.params.type);
+	switch (req.params.type) {
+		case "user":
+		case "item":
+		case "monster":
+			res.render("admin/form", { user : req.user, type: req.params.type });
+			break;
+		default:
+			res.redirect("/admin");
+			break;
+	}
+});
+
+router.get("/edit/:type/:id", function(req, res, next) {
+	// TODO: investigate removing duplicate code
+	switch (req.params.type) {
+		case "user":
+			User.findOne({
+				where: {
+					id: req.params.id
+				}
+			}).then(function(user: User){
+				if (user != null)
+					res.render("admin/form", { user : req.user, type: req.params.type, data: user });
+				else
+					res.redirect("/admin");
+			});
+			break;
+		case "item":
+			Item.findOne({
+				where: {
+					id: req.params.id
+				}
+			}).then(function(item: Item){
+				if (item != null)
+					res.render("admin/form", { user : req.user, type: req.params.type, data: item });
+				else
+					res.redirect("/admin");
+			});
+			break;
+		case "monster":
+			MonsterType.findOne({
+				where: {
+					id: req.params.id
+				}
+			}).then(function(monsterType: MonsterType){
+				if (monsterType != null)
+					res.render("admin/edit", { user : req.user, type: req.params.type, data: monsterType });
+				else
+					res.redirect("/admin");
+			});
+			break;
+		default:
+			res.redirect("/admin");
+			return;
+	}
 });
 
 module.exports = router;
