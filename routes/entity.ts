c5b2b6866b633897f24a714a40e30e5ce038ecf6
shazard <shaunh1096@gmail.com>
2017-09-10 20:18:47 -0400

    updated routes for entity view

--- routes/entity.ts
+++ routes/entity.ts
@@ -1,15 +1,21 @@
 // takes what we need from the express library and imports into this page
 import {Router} from "express";
+import Entity from "../models/Entity";
 
 let router = Router();
 
-/* GET home page. */
 router.get("/:id", function(req, res, next) {
-	if (req.params.id) {
-		res.render("entityView", { user : req.user, entity : req.params.id});
-	} else {
-		res.redirect("auth/login");
-	}
-});
 
+	Entity.findOne({
+		where: {
+			id: req.params.id
+		}
+	}).then(function(result){
+		if (req.params.id) {
+			res.render("entity", { entity: Entity});
+		} else {
+			res.redirect("/");
+		}
+	});
+});
 module.exports = router;
